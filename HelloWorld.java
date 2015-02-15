13cabb481b4fbbc074b39392d4d60af368d0f3e1
GeronimoOM <olehmatsuk@gmail.com>
2015-02-15 11:28:50 +0200

    Message changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Goodbye, world!");
+		System.out.println("Greetings, fellow citizens of the Earth!");
 
 	}
 
